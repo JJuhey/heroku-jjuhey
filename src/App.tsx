e7ff3499b6c5bb42099af7182d56e83c71789f1c
JJuhey <dallicjh@gmail.com>
2021-02-24 22:57:26 +0900

    Install typescript & Apply

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC =() => {
   return (
     <div className="App">
-      HELLO JJUHEY's WORLD!
+      HELLO JJUHEY's WORLD
     </div>
   );
 }
