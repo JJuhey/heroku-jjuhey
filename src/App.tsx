8789f7bd9e307079920e415f2ae3d8a0183eb9f8
JJuhey <dallicjh@gmail.com>
2021-02-25 20:08:40 +0900

    Apply react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+
+import MainPage from './components/pages/MainPage'
+import LoginPage from './components/pages/LoginPage'
+
 import './App.css';
 
 const App: React.FC =() => {
   return (
     <div className="App">
       HELLO JJUHEY's WORLD
+      <Router>
+        <Route path='/main' component={MainPage} />
+        <Route path='/sign' component={LoginPage} />
+      </Router>
     </div>
   );
 }
